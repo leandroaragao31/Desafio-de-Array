97831aabeae89a7249b0b113f5169fed1147772d
leandroaragao31 <leandroaragao31@gmail.com>
2021-05-23 04:16:30 -0300

    ultilizando array para criar uma lista de itens e organizar uma lista de numeros

---
+++ script.js
@@ -0,0 +1,15 @@
+let itens =["cadeira", "mesa", "notebook", "cordão", "quadro"]
+
+itens.unshift("Porta")
+itens.pop()
+itens.push("Borracha", "Caneta")
+itens.shift()
+
+console.log(itens)
+
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+
+numbers.sort()
+
+console.log(numbers)
